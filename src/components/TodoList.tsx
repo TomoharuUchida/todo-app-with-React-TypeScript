5f5407b1e62fe33fa386d783abb3c7bba5186840
TomoharuUchida <tomou0711@gmail.com>
2022-06-29 23:30:04 +0900

    edit input にuseRefを追加 Completed tasksのdivを追加

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,16 +9,33 @@ interface Props{
 }
 
 export const TodoList: React.FC<Props> = ({todos, setTodos}) => {
-  return (
-      <div className="todos">
-          {todos.map((todo) => (
-              <SingleTodo
-                  todo={todo}
-                  key={todo.id}
-                  todos={todos}
-                  setTodos={setTodos}
-              />
-          ))}
+    return (
+        <div className="container">
+            <div className="todos">
+                <span className="todos__heading">Active tasks</span>
+                      {todos.map((todo) => (
+                            <SingleTodo
+                                todo={todo}
+                                key={todo.id}
+                                todos={todos}
+                                setTodos={setTodos}
+                            />
+                        ))}
+            </div>
+            <div className="todos remove">
+                <span className="todos__heading">Completed tasks</span>
+                      {todos.map((todo) => (
+                            <SingleTodo
+                                todo={todo}
+                                key={todo.id}
+                                todos={todos}
+                                setTodos={setTodos}
+                            />
+                        ))}
+            </div>
       </div>
+    //   <div className="todos">
+    
+    //   </div>
   )
 }
